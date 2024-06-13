53e8b9b3a46c4515fd5748aa4732e185129f9a15
JuanMartinMena <Juanmartinmenac@gmail.com>
2024-06-13 14:41:38 +0000

    4

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("%lu 1\n", sizeof(char));
-	printf("%lu 4\n", sizeof(int));
-	printf("%lu 4\n", sizeof(long int));
-	printf("%li 8\n", sizeof(long long int));
-	printf("%lu 4\n", sizeof(float));
+	printf("Size of a char:%u\n", sizeof(char));
+	printf("Size of an int:%u\n", sizeof(int));
+	printf("Size of a long int:%u\n", sizeof(long int));
+	printf("Size of a long long int:%u\n", sizeof(long long int));
+	printf("Size of a float:%u\n", sizeof(float));
 	return 0;
 }
 
