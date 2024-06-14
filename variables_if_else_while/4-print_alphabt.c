2509c00935c7bd64e339d889fca79dc55581b217
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-14 17:18:44 +0000

    Print a-z whitout q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char inicio = 'a';
+
+	while (inicio < 'z')
+		if (inicio != 'e' && inicio != 'q')
+{
+		putchar(inicio);
+}
+{
+		inicio++;
+}
+	return (0);
+}
