ab21e6fb81d8acbc11e894cada5ceb2a7c61d636
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-14 22:23:03 +0000

    numeros en base 10 pero con comas y espacios

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int numero;
 
-	for (numero = 0; numero <= 8; numero++)
+	for (numero = 0; numero <= 9; numero++)
 {
 	putchar(numero + '0');
+	if (numero != 9)
+{
 	putchar(',');
 	putchar(' ');
+}
 }
 	putchar('\n');
 
