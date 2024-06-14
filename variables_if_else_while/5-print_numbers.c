225b51e5caf838c03501c52982ffa2f28fab39c3
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-14 18:37:12 +0000

    3

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Base10 = '1';
+
+	for (Base10 = 1; Base10 <= 9; Base10++)
+{
+	putchar(Base10);
+}
+	putchar('\n');
+
+	return (0);
+}
