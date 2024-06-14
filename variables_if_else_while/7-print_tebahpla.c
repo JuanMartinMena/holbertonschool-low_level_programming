d74cd7ea4e0e6e0353a4a9fed3383f9942e40e89
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-14 19:16:52 +0000

    seguir en casa

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letra
+
+	for (letra = 'z'; letra <= 'a'; letra++)
+{
+	putchar(letra + '0');
+}
+	putchar("\n");
+
+	return (0)
+}
