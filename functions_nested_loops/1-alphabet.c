206069a4fb7f5beccc23f602007ef9b8f3c84d09
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-15 23:01:11 +0000

    alphabet whit void and 2 _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * print_alphabet - prints
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char inicio = 'a';
+
+	for (inicio = 'a'; inicio <= 'z'; inicio++)
+{
+	_putchar(inicio);
+}
+	_putchar('\n');
+}
