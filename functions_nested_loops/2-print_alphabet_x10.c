d17c5d74601083b764242eb1cd5b86463855e8e5
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-16 20:32:11 +0000

    haber

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,20 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - print
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int repetir;
+	char alphabet;
 
+	for (repetir = 0; repetir < 10; repetir++)
+{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	_putchar(alphabet);
+}
+	_putchar('\n');
+}
+}
