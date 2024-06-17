fa5d158da788f78425ae01434903fc1407e0fb74
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 16:39:33 +0000

    3

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table - print
+ *
+ * Return: tablas
+ */
+void times_table(void)
+{
+	int col1;
+	int col2;
+
+	for (col1 = 0; col1 <= 9; col1++)
+	{
+		for(col2 = 0; col2 <= 9; col2++)
+		{
+
