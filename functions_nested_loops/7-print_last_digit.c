e3e5d77970d2b9afb6581a305dd15d106189a7f0
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 13:34:23 +0000

    Last Digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - print
+ * @n: numero random
+ *
+ * Return: Last number
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	return (last);
+}
+
