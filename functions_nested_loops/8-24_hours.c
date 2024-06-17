e40259ac9c3f91915e724a485fa5f1caa0b09b65
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 14:04:22 +0000

    Hora

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * jack_bauer - print
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int T;
+
+	for (T = 0000; T <= 2359; T++)
+{
+	_putchar(T);
+	_putchar('\n');
+}
+}
