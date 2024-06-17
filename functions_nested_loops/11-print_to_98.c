0beb18146a5703a11d215285f7e84b8502a7844c
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 17:50:32 +0000

    3

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - print
+ * 
+ *
+ */
+void print_to_98(int n);
+{
+	if (n <= 98)
+{	
+	for (i = n; i <= 98; i++)
+}
+	while (n < 98)
+{
+
+}
+	while (n == 98)
+{
+
+}
+}
