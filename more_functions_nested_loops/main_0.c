0c6b588cc1e22ff2f0585ac765c8335c1a54521b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 23:29:52 +0000

    UpLetter

--- more_functions_nested_loops/main_0.c
+++ more_functions_nested_loops/main_0.c
@@ -1,1 +1,13 @@
-int _isupper(int c);
+#include <stdio.h>
+#include "main.h"
+
+int main(void) {
+    char c;
+
+    for (c = 'A'; c <= 'z'; c++) {
+        printf("%c: %d\n", c, _isupper(c));
+    }
+
+    return 0;
+}
+
