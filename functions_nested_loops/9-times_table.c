cbc0ce2e472208ed2be86e6b0f952b4cbabd036d
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 22:53:37 +0000

    Tablass

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,19 +6,37 @@
  */
 void times_table(void)
 {
-	int c;
+	int a = 0;
 	int b;
-	int a;
+	int c;
+	int d;
+	int e;
 
-	for (c = 0; c <= 9; c++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			a = b * c;
-
-			if (
-				
-			
+			c = a * b;
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(e + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(c + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
