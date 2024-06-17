e803acf0aeb279b2542930a7a4287a71a00f3527
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-17 20:08:09 +0000

    3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,35 +15,10 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			a = b * c;
-			if (b != 9)
-			{
-				if (a >= 10)
-				{
-				_putchar('0' + a / 10);
-				_putchar('0' + a % 10);
-				_putchar(',');
-				_putchar(' ');
-				}
-				else
-				{
-				_putchar('0' + a);
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				}
-			}
-			else
-			{
-				if (a >= 10)
-				{
-				_putchar('0' + a / 10);
-				_putchar('0' + a % 10);
-				}
-				else
-				{
-				_putchar('0' + a);
-				}
-			}
+
+			if (
+				
+			
 		}
 	}
 }
