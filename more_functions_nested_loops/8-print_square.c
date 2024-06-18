bdb92f98cc15b025247dbd079e8363ce563a8cf3
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-18 13:59:25 +0000

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - print
+ *
+ * @size: variable
+ * Return: void
+ */
+void print_square(int size)
+{
+	int nlineas;
+	int nh;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (nlineas = 0; nlineas <= size; nlineas++)
+		{
+			for (nh = 0; nh <= size; nh++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
