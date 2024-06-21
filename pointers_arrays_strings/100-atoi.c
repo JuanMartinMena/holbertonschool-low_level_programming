07eb2cf1c462085e30e108f1461e69209a6c2870
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 18:20:27 +0000

    casa

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,9 +10,27 @@
 int _atoi(char *s)
 {
 	int largo = 0;
-
-	while (s[largo] != '\0')
+	char menos = '-';
+	char mas = '+';
+	int numero = 
+	while (s[largo] != '\0') //esto es para ver el largo del string
 	{
 		largo++;
 	}
-	for (
+	if (largo == 65 - 90 || largo == menos, mas) //si los valores del string son numeros/+/- que haga lo de adentro 
+	{
+		if () //si uno es mayor al otro que me lo cambie
+		{
+		//printear signo de menos
+		}
+		else
+		{
+		//printear printear singo de mas
+		}
+		//printear numero
+	}
+	else //si no es numero/+/- retorne 0
+	{
+	return (0);
+	}
+}
