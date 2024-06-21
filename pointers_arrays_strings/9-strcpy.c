5562500bccd56c0b016c0cc9fb04c567bb4707a5
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 14:29:59 +0000

    3

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		largo++;
 	}
-	for (dest[largo] = 0; dest[largo] <= largo; largo++)
+	for (dest[largo] = 0; dest[largo] != '\0'; largo++)
 	{
 	dest[largo] = src[largo];
 	}
