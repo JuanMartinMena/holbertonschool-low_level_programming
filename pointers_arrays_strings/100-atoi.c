73316e0efef0c42e2c27b2ebd431ee211acc7997
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 18:24:40 +0000

    casa

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,12 +12,12 @@ int _atoi(char *s)
 	int largo = 0;
 	char menos = '-';
 	char mas = '+';
-	int numero = 
+	int numero = //todos los valores de los numeros en la tabla ascci
 	while (s[largo] != '\0') //esto es para ver el largo del string
 	{
 		largo++;
 	}
-	if (largo == 65 - 90 || largo == menos, mas) //si los valores del string son numeros/+/- que haga lo de adentro 
+	if (largo == numero, menos, mas) //si los valores del string son numeros/+/- que haga lo de adentro 
 	{
 		if () //si uno es mayor al otro que me lo cambie
 		{
