d3bc05e7e9229297246191d590326e0c6c66bceb
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 13:05:48 +0000

    3

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * *_strcpy - print
+ *
+ * @dest: string2
+ * @src: string1
+ *
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int largo = 0;
+	int largo2 = 0;
+	int mayor;
+	int menor;
+
+	while (src[largo] != '\0')
+	{
+		largo++;
+	}
+	while (dest[largo2] != '\0')
+	{
+		largo2++;
+	}
+	mayor = largo - largo2;
+	menor = largo2 - largo;
+	if (largo == largo2)
+	{
+		dest[largo2] = src[largo];
+	}
+	else if (largo >= largo2)
+	{
+		dest[largo2] = src[largo] - mayor;
+	}
+	else
+	{
+		dest[largo2] = src[largo] - menor;
+	}
+	return (dest);
+}
