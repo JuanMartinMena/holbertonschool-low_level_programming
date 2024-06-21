62076e181737cfb163e517131d0311c59c85d9b9
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 22:18:25 +0000

    casa

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,10 +13,25 @@ int _atoi(char *s)
 	char menos = '-';
 	char mas = '+';
 	int numero = (x >= 48 && x <= 57)//todos los valores de los numeros en la tabla ascci
-	while (s[largo] != '\0') //esto es para ver el largo del string
+	while (s[largo] != '\0')//esto es para ver el largo del string
 	{
 		largo++;
 	}
+	if (menos % 2 != 0)
+	{
+		_putchar(menos);
+	}
+		
+
+
+
+
+
+
+
+
+
+
 	if (largo == numero, menos, mas) //si los valores del string son numeros/+/- que haga lo de adentro 
 	{
 		if () //si uno es mayor al otro que me lo cambie
@@ -34,4 +49,4 @@ int _atoi(char *s)
 	return (0);
 	}
 }
-// tengo que hacer un loop para que te vaya imprimiendo de 1 en 1 los numeros, entonces afuera de eso tenes que hacer lo del mas u menos
+// tengo que hacer un loop para que te vaya imprimiendo de 1 en
