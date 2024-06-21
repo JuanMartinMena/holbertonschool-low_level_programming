e46bdb7957033e716c1117e03b2d3a488fcc104b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-21 16:25:44 +0000

    3

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _atoi - print
+ *
+ * @s: variable
+ *
+ *
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int largo = 0;
+
+	while (s[largo] != '\0')
+	{
+		largo++;
+	}
+	for (
