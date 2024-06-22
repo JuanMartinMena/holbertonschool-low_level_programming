a5785975e8e09b736fc508028602b94b2efae9e5
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-22 15:16:32 +0000

    rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,23 @@
  */
 void rev_string(char *s)
 {
-	
+	int largo = 0;
+	char *inicio, *final, variable;
+
+	while (s[largo] != '\0')
+	{
+		largo++;
+	}
+	inicio = s;
+	final = s + largo -1;
+
+	while (final > inicio)
+	{
+		variable = *inicio;
+		*inicio = *final;
+		*final = variable;
+
+		inicio++;
+		final--;
+	}
+}
