dcc5b561d825d7266eb58ad29508076b608a7f52
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-24 17:41:18 +0000

    seguir en casa

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *leet - print
+ *
+ * @str: string
+ *
+ * Return: void
+ */
+char *leet(char *str)
+{
+	//variable de letras
+	//variable de numeros
+	//recorrer array
+	//cambiar letras por numeros
+	//retornar
