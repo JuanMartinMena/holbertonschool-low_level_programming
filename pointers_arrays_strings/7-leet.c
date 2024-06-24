0ec0657fc7dbfa93a76d9d1687674ca060122194
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-24 23:26:55 +0000

    1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,8 +8,21 @@
  */
 char *leet(char *str)
 {
-	//variable de letras
-	//variable de numeros
-	//recorrer array
-	//cambiar letras por numeros
-	//retornar
+	char letras[] = "aAeEoOtTlL";
+	char numeros[] = "4433007711";
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letras[j] != '\0'; j++)
+		{
+			if (str[i] == letras[j])
+			{
+				str[i] = numeros[j];
+			}
+		}
+	}
+	return (str);
+}
+
