a58b12736abf8bf7d00e73a4a1c5f4ca5b0128d2
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-24 01:16:02 +0000

    reverse intarray

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - array_alrevez
+ *
+ * @a: array
+ * @n: numero_en_el_array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int inicio = 0;
+	int final = n - 1;
+	int valorinicio = a[inicio];
+
+	while (inicio < final)
+	{
+		a[inicio] = a[final];
+		a[final] = valorinicio;
+		inicio++;
+		final--;
+	}
+}
