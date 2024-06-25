fb4e28587d693dcfc383923a2cef88dc5bdb7288
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-25 22:05:44 +0000

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,8 +17,10 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i*size + i);
+	}
+	for (i = 0; i < size; i++)
+	{
 		sum2 += *(a + i*size + (size - 1 - i));
 	}
-	printf("%d %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
-
