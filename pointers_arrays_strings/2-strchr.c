74380756142aabf5652dfdba815944df67017fbb
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-25 13:26:52 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,15 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c)
-		{
-			return (s);
-			s++;
-		}
-		else
+		if (c == '\0')
 		{
 			return ('\0');
 		}
+		else
+		{
+			return (s);
+		}
+		s++;
 	}
 	return (s);
 }
