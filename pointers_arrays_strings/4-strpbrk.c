df97e4983b83cf6f7672e3f84c9033a5d2a51355
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-25 20:47:19 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strpbrk - print
+ *
+ * @s: string1
+ * @accept: string2
+ *
+ * Return: void
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
