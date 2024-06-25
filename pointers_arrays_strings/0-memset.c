28ee39dfa9087c1b349c84fc7e02ee1d967725a1
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-25 12:10:47 +0000

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * -memset - print
+ *
+ * @s: string
+ * @b: bytes
+ * @n: Number_of_bytes_to_fill
+ *
+ * Return: num of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		*s = b;
+	}
+	return (s);
+}
