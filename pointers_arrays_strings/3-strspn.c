298ea96c8ce1de312d8275f2dd34fd1c5828717c
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-25 20:24:11 +0000

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * int _strspn - print
+ *
+ * @s: string1
+ * @accept: string2
+ *
+ * Return: void
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int match;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		match = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+			}
+		}
+		if (match == 0)
+		{
+			return (i);
+		}
+	}
+	return (i);
+}
