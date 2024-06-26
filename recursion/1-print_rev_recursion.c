7dc7f1cfb0e6c431092fcab379309123d9193ae1
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-26 16:50:19 +0000

    casa

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -10,9 +10,13 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
+		if (*s >= 0)
+		{
+			_putchar(s*);
+			s--;
+		}
+		return
 	}
-	_putchar(*s);
 	_print_rev_recursion(s + 1);
+
 }
