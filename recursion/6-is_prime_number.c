b088d36fdb19814937df4f1605e6b16b4c6f1aad
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-27 18:36:33 +0000

    primo

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,6 +6,31 @@
  *
  * Return: void
  */
+int operacion(int n, int divisor)
+{
+	if (divisor > n / 2)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	return operacion(n, divisor + 1);
+}
 int is_prime_number(int n)
 {
-
+	if (n < 0)
+	{
+		n = -n;
+	}
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return operacion(n, 2);
+}
