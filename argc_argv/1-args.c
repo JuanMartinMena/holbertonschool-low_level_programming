eefe38007eda61a7dd2f0a10fd0dd9aae7bec1a5
JuanMartinMena <juanmartinmenac@gmail.com>
2024-06-28 17:18:21 +0000

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ * @argc: numero de argumentos
+ * @argv: string de argc
+ *
+ * Return: 0 (Succcess)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
