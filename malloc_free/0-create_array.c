c2508de671d79ab37eb3a28396b9d6e947554e7c
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-01 18:38:08 +0000

    commentado

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,20 +13,20 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i = 0;
 
-	arr = malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char)); // aca lo que se esta haciendo es crear el espacio de memoria justo para size.
 
-	if (size == 0)
+	if (size == 0) // aca lo que dijo es que si size es 0 que me retorne NULL.
 	{
 		return (NULL);
 	}
-	if (arr == NULL)
+	if (arr == NULL) // aca lo que se dice es que si el array no tiene nada solo el NULL que me retorne NULL.
 	{
 		return (NULL);
 	}
-	while (i < size)
+	while (i < size) // Se hace un bucle para que la variable i (que tiene valor 0) avance hasta el tamaÃo de size - 1(\0).
 	{
 		arr[i] = c;
 		i++;
 	}
-	return (arr);
+	return (arr); // Se retorna arr porque queremos ver el array creado.
 }
