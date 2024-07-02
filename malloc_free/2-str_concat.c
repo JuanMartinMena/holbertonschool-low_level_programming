b587639234b2e6a8c7f44e7619f3f12daf717269
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-02 13:02:23 +0000

    str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - hace que 2 string se concatenen
+ *
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: string concatenado
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0;
+	char *final;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	final = malloc((i + j + 1) * sizeof(char));
+	
+	if (final == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		final[k] = s1[k];
+	}
+	for (k = 0; k < j; k++)
+	{
+		final[k + i] = s2[k];
+	}
+
+	final[i + j] = '\0';
+	
+	return (final);
+}
