de07a3b38991864986a8e86b6cdd226941609012
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-02 13:16:10 +0000

    str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,6 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0;
 	char *final;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[i] != '\0')
 	{
 		i++;
