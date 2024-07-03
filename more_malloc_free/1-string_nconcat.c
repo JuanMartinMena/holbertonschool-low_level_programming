b98ab8d7cb254ed08befd9ea9a3002726b48ef9b
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-03 19:30:39 +0000

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, k = 0, largo;
 	char *final;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	while (s1[i] != '\0')
 	{
 		i++;
