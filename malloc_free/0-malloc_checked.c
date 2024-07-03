f3a09e2a2beb8840754251c6284dbe0cda2d636a
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-03 17:30:41 +0000

    3

---
+++ malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib,h>
+/**
+ * malloc_checked - 
+ *
+ *
+ *
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int * memoria;
+
+	memoria = malloc(b * sizeof(unsigned int));
+
+	if (memoria = NULL)
+	{
+		exit(memoria);
+	}
+}
