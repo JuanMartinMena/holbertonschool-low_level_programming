151ad93ec057664a2943567d28e010921ee33a7e
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-04 13:50:48 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,40 +3,36 @@
 /**
  * _calloc - funcion que aloja memoria para un array
  *
- * @nmemb: filas
- * @size: columnas
+ * @nmemb: cantidad de elementos
+ * @size: tamaño de elementos
  *
  * Return: array doble inicializado con memoria 0
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int **array, i = 0, j = 0;
+	unsigned int i = 0, total = 0;
+	void *puntero;
+	char *limpieza;
 
 	if (nmemb <= 0 || size <= 0)
 	{
 		return (NULL);
 	}
-	array = malloc(nmemb * sizeof(unsigned int));
-	if (array == NULL)
+
+	total = nmemb * size;
+
+	puntero = malloc(total);
+
+	if (puntero == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+
+	limpieza = (char *) puntero;
+
+	for (i = 0; i < total; i++)
 	{
-		array[i] = malloc(size * sizeof(unsigned int));
-		if (array[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(array[j]);
-			}
-			free(array);
-		return (NULL);
-		}
+		limpieza[i] = 0;
 	}
-	for (j = 0; j < size; j++)
-	{
-		array[i][j] = 0;
-	}
-	return (array);
+	return (puntero);
 }
