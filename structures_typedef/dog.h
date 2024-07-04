95ced318a5e6437f7e91eb576e0d63a959a545d6
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-04 16:56:12 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
+ * struct dog - info del perro
  *
+ * @name: puntero para el nombre del perro
+ * @age: edad del perro
+ * @owmer: puntero para el dueno del perro
  *
- *
- *
- *
+ * Return: info del perro
  */
 struct dog
 {
