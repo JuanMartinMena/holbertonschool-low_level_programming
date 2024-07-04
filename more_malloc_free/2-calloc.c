13106cbe65a1eb0842644497eac89cd977b29b96
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-04 15:09:01 +0000

    comentado

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,25 +13,25 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0, total = 0;
 	char *limpieza, *puntero;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb <= 0 || size <= 0) // Si cualquiera de las 2 variables es menor o igual a 0 retornamos NULL.
 	{
 		return (NULL);
 	}
 
-	total = nmemb * size;
+	total = nmemb * size; // Definimos la variable total como la multiplicacion entre "nmemb" y "size".
 
-	puntero = malloc(total);
+	puntero = malloc(total); // Reservamos memoria para el puntero "puntero" de cantidad de la variable "total".
 
-	if (puntero == NULL)
+	if (puntero == NULL) // Si la reserva de memoria falla se retorna NULL.
 	{
 		return (NULL);
 	}
 
-	limpieza = puntero;
+	limpieza = puntero; // Asignamos "limpieza" a "puntero".
 
-	for (i = 0; i < total; i++)
+	for (i = 0; i < total; i++) //bucle para recorrer el puntero "limpieza".
 	{
-		limpieza[i] = 0;
+		limpieza[i] = 0; // Se asigna el valor 0 para cada dato de limpieza.
 	}
-	return (puntero);
+	return (puntero); // Retornamos el puntero "puntero".
 }
