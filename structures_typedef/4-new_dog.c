1c636fd91e1534cfa6ab4b9e965c81cd15842863
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-06 19:37:41 +0000

    new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,2 +1,67 @@
 #include <stdlib.h>
-#include <str
+#include "dog.h"
+
+/**
+ * _strdup - duplica string
+ *
+ * @str: string
+ *
+ * Return: puntero al string duplicado
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i = 0, largo = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (largo = 0; str[largo] != '\0'; largo++)
+	{
+	}
+	dup = malloc(sizeof(char) * (largo + 1));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < largo; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[largo] = '\0';
+	return (dup);
+}
+/**
+ * new_dog - cre nuevo perro
+ *
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner: dueño del perro
+ *
+ * Return: puntero a la nueva structur dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = _strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = _strdup(owner);
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	d->age = age;
+	return (d);
+}
