fa315f9d739d47096da701a8360a50f23be9a5df
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-08 18:28:01 +0000

    commentado

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,15 +3,15 @@
 /**
  * print_name - escribe un nombre
  *
- * @name: array de caracteres que contiene el nombre
- * @f: puntero a la funcion de tipo char
+ * @name: un puntero que apunta al nombre que se va a imprimir
+ * @f: un puntero a funcion char
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name != NULL && f != NULL) // Mientras que "f" y "name" no sean NULL.
 	{
-		f(name);
+		f(name); // Se llama a la funcion apuntada por "f" y se le pasa "name" como argumento
 	}
 }
