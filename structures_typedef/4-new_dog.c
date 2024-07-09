bbe0a9dea9de14f9438b67a771c63dd8bffd5aeb
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-09 13:57:57 +0000

    commentando

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,24 +13,24 @@ char *_strdup(char *str)
 	char *dup;
 	int i = 0, largo = 0;
 
-	if (str == NULL)
+	if (str == NULL) // Si el string es NULL retorna NULL
 	{
 		return (NULL);
 	}
-	for (largo = 0; str[largo] != '\0'; largo++)
+	for (largo = 0; str[largo] != '\0'; largo++) // Se averigua el largo del string
 	{
 	}
-	dup = malloc(sizeof(char) * (largo + 1));
-	if (dup == NULL)
+	dup = malloc(sizeof(char) * (largo + 1)); // Se reserva espacio del tamaño del string + el caracter nulo y se almacena e "dup"
+	if (dup == NULL) // Si la reserva falla retorna NULL
 	{
 		return (NULL);
 	}
-	for (i = 0; i < largo; i++)
+	for (i = 0; i < largo; i++) // Bucle para copiar cada valor de la cadena "str" a "dup"
 	{
 		dup[i] = str[i];
 	}
-	dup[largo] = '\0';
-	return (dup);
+	dup[largo] = '\0'; // Se le agrega al final de la cadena de "dup" un caracter nulo
+	return (dup); // Se retorna la cadena duplicada
 }
 /**
  * new_dog - cre nuevo perro
@@ -43,30 +43,30 @@ char *_strdup(char *str)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *new_name;
-	char *new_owner;
+	dog_t *d; // puntero que apunta a la estructura "dog_t"
+	char *new_name; // puntero que aputa al nombre de la copia del perro
+	char *new_owner; // puntero que apunta al nombre de la copia del dueño
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	d = malloc(sizeof(dog_t)); // Se reserva memoria para la estructura de tipo "dog_t" en el puntero "d"
+	if (d == NULL) // Si falla la reserva retorna NULL
 	{
 		return (NULL);
 	}
-	new_name = _strdup(name);
-	if (new_name == NULL)
+	new_name = _strdup(name); // duplicamos el string "name" a al string "new_name"
+	if (new_name == NULL) // Si no ahi nada que copiar libera la memoria y retorna NULL
 	{
 		free(d);
 		return (NULL);
 	}
-	new_owner = _strdup(owner);
-	if (new_owner == NULL)
+	new_owner = _strdup(owner); // Duplicamos el string "owner" al string "new_owner"
+	if (new_owner == NULL) // Si no ahi nada que copiar libera la memoria de "new_name" y la de "new_owner" y retorna NULL
 	{
 		free(new_name);
 		free(d);
 		return (NULL);
 	}
-	d->name = new_name;
-	d->owner = new_owner;
-	d->age = age;
-	return (d);
+	d->name = new_name; // Asignamos el valor de "new_name" a "name" de la copia
+	d->owner = new_owner; // Asignamos el valor de "new:owner" a "owner" de la copia
+	d->age = age; // asgina el valor de "age" a "age" de la copia
+	return (d); // retorna nuevo perro
 }
