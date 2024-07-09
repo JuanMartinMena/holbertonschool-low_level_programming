4231c11f662f5ab2d70bbbb0b21e8227597c2eae
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-09 13:17:54 +0000

    commentando

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,24 +10,24 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d == NULL) // Si no encuentra valor para "d" retorna NULL
 	{
 		return;
 	}
-	if (d->name == NULL)
+	if (d->name == NULL) // Si el "name" dentro de la structura "dog" es NULL, entonces imprime "Name: (nil)" y un salto de linea.
 	{
 		printf("Name: (nil)\n");
 	}
-	else
+	else // Si el "name" dentro de la structura "dog" existe, entonces imprime "Name: (el valor de name)" y un salto de linea.
 	{
 		printf("Name: %s\n", d->name);
 	}
-	printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
+	printf("Age: %.6f\n", d->age); // Imprime el valor de "age" hasta con 6 decimales
+	if (d->owner == NULL) // Si el "owner" dentro de la structura "dog" es NULL, entonces imprime "Owner: (nil)" y un salto de linea.
 	{
 		printf("Owner: (nil)\n");
 	}
-	else
+	else // Si "owner" dentro de la structura "dog" existe, entonces imprime "Onwer: (el dato de owner)" y un salto de linea.
 	{
 		printf("Owner: %s\n", d->owner);
 	}
