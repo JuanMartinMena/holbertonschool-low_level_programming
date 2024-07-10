a9b7be848e306be899677411099287be3f6aafba
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-10 14:10:08 +0000

    voy

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Funcion para elegir la operacion
+ *
+ * @s: operador pasado
+ *
+ * Return: puntero a la funcion correspondiente del operador
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = 
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+		{
+			return (ops[i]->f);
+		}
+		i++
+	}
+	printf("Error\n");
+	exit(99);
+}
+
