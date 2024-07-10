b5f65eb694c5bbbd0daeb0f989194cb2bc633602
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-10 13:22:23 +0000

    5 funciones hechas

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -13,6 +13,14 @@ int op_add(int a, int b)
 	sumados = a + b;
 	return (sumados);
 }
+/**
+ * op_sub - resta
+ *
+ * @a: numero1
+ * @b: numero2
+ *
+ * Return: numeros restados
+ */
 int op_sub(int a, int b)
 {
 	int restados = 0;
@@ -20,6 +28,14 @@ int op_sub(int a, int b)
 	restados = a - b;
 	return (restados);
 }
+/**
+ * op_mul - multiplicacion
+ *
+ * @a: numero1
+ * @b: numero2
+ *
+ * Return: numeros multiplicados
+ */
 int op_mul(int a, int b)
 {
 	int multiplicados = 0;
@@ -27,6 +43,14 @@ int op_mul(int a, int b)
 	multiplicados = a * b;
 	return (multiplicados);
 }
+/**
+ * op_div - division
+ *
+ * @a: numero1
+ * @b: numero2
+ *
+ * Return: numero divididos
+ */
 int op_div(int a, int b)
 {
 	int divididos = 0;
@@ -34,6 +58,14 @@ int op_div(int a, int b)
 	divididos = a / b;
 	return (divididos);
 }
+/**
+ * op_mod - modulo
+ *
+ * @a: numero1
+ * @b: numero2
+ *
+ * Return: modulo de a % b
+ */
 int op_mod(int a, int b)
 {
 	int modulo = 0;
