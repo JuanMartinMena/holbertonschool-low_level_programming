8112ab9b60b7ab62c77e07a30464ed91d7215dc0
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-10 18:27:11 +0000

    calculadora

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -25,9 +25,9 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 		{
-			return (ops[i]->f);
+			return (ops[i].f);
 		}
-		i++
+		i++;
 	}
 	printf("Error\n");
 	exit(99);
