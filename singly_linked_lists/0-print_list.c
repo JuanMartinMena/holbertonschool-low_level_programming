b7d90ef7fd20029507bf1e83dee2709bfb8a4c39
JuanMartinMena <juanmartimenac@gmail.com>
2024-07-29 13:32:51 +0000

    print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - funcion que printea el cotenido de los nodulos
+ *
+ * @h: puntero que apunta a la structura size_t
+ *
+ * Return: numero de nodulos
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
