a531016e239501785b5b88e6c7bedb1aba6bae45
JuanMartinMena <juanmartinmenac@gmail.com>
2024-08-02 15:59:28 +0000

    clear_bit

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * set_bit - Establece el valor del bit a 1 en un índice dad
+ * clear_bit - Establece el valor del bit a 0 en un índice dad
  *
- * @n: Puntero al número en el que se establecerá el b
- * @index: El índice del bit, comenzando desde 
+ * @n: Puntero al número en el que se establecerá el bit
+ * @index: El índice del bit, comenzandodesde 0
  *
- * Return: 1 si funcionó, o -1 si ocurrió un eor
+ * Return: 1 si funcionó, o -1 si ocurrió unrror
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
