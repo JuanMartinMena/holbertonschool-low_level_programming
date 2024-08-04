f7956d875b5d59da0e49fac1842c4c9e47c80fc3
JuanMartinMena <juanmartinmenac@gmail.com>
2024-08-04 21:13:06 +0000

    changes to the logic of the code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
+	char fd_str[10];
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", NULL);
@@ -35,18 +36,35 @@ int main(int argc, char *argv[])
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+	{
+		close(fd_from);
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
 	}
 	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+	{
+		snprintf(fd_str, sizeof(fd_str), "%d", fd_from);
+		error_exit(100, "Error: Can't close fd %d\n", fd_str);
+	}
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+	{
+		snprintf(fd_str, sizeof(fd_str), "%d", fd_to);
+		error_exit(100, "Error: Can't close fd %d\n", fd_str);
+	}
 	return (0);
 }
